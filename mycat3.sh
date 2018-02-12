85d65474ee7f487ad55f4a7c37c075d834a9c29b
debonairdreams <bebrooks@albany.edu>
2018-02-11 23:34:49 -0500

    Update mycat3.sh

--- mycat3.sh
+++ mycat3.sh
@@ -1,1 +1,9 @@
+if  [[ $1 = "-u" ]]; then
+    echo "THIS IS A TEST"
+else [[ $1 = "-l"]]
+    echo "this is a test"
+ else [[ $1 = "-t"]]
+        echo "This Is A Test"
+else echo "Usage: mycat3 [-u|-l|-t] FILE ..."
 
+fi
